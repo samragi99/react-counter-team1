9dc6b46240fb9913e2fd013d728934434ca48d75
samragi99 <143216421+samragi99@users.noreply.github.com>
2023-09-20 13:20:25 -0500

    create counter app and apply js

--- src/counter.jsx
+++ src/counter.jsx
@@ -2,11 +2,28 @@ import "./counter.css";
 import React, { useState } from "react";
 
 function Counter() {
+ 
+  const [count, setCount] = useState(0)
+
+
   return (
-    <>
-      <div></div>
-    </>
-  );
+    <div>
+<div className='box'>
+<button className='btn'> Count {count}</button>
+ <br/> <br/>
+
+
+<button onClick={() => setCount((count) => count + 1)} className='btn1'>Increment</button>
+<button onClick={() => setCount((count) => count - 1)} className='btn2'>Decrement</button>
+<br/><br/><br/><br/><br/><br/>
+<button className='btn3'><marquee >TEAM 1</marquee></button>
+
+</div>
+
+
+    </div>
+  )
 }
+
 
 export default Counter;
